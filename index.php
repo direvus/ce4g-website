6da56cbd42941a49dfc8064763769826e3f30878
Brendan Jurd <direvus@gmail.com>
2015-08-03 02:56:01 +1000

    Convert the Home page into a CouchCMS template.
    I have defined six editable regions, for each of the headings and
    content sections for the three content columns on the Home page.

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php require_once('couch/cms.php'); ?>
+<cms:template title="Home" />
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -35,7 +37,7 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-3">
-          <img src="http://www.ce4g.org.au/images/logo.jpg" alt="CE4G logo" class="img-thumbnail" style="height: 300px;">
+          <img src="http://www.ce4g.org.au/images/logo.jpg" alt="CE4G logo" class="img-thumbnail">
         </div>
         <div class="col-sm-9">
           <h1>Community Energy 4 Goulburn</h1>
@@ -47,16 +49,34 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-4">
-          <h2>Who we are</h2>
+          <h2>
+            <cms:editable name="col1_heading" type="richtext">
+            Who we are
+            </cms:editable>
+          </h2>
+          <cms:editable name="col1_content" type="richtext">
           <p>...</p>
+          </cms:editable>
         </div>
         <div class="col-sm-4">
-          <h2>What we do</h2>
+          <h2>
+            <cms:editable name="col2_heading" type="richtext">
+            What we do
+            </cms:editable>
+          </h2>
+          <cms:editable name="col2_content" type="richtext">
           <p>...</p>
+          </cms:editable>
         </div>
         <div class="col-sm-4">
-          <h2>Some other thing</h2>
+          <h2>
+            <cms:editable name="col3_heading" type="richtext">
+            Some other thing
+            </cms:editable>
+          </h2>
+          <cms:editable name="col3_content" type="richtext">
           <p>...</p>
+          </cms:editable>
         </div>
       </div>
     </div>
@@ -68,3 +88,4 @@
     </footer>
   </body>
 </html>
+<?php COUCH::invoke(); ?>
